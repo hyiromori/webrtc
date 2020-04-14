d61cdc1386d0f228ef577f2fecb029fb2ef90ebe
hyiromori <hyiromori@gmail.com>
2020-04-15 08:16:19 +0900

    Goodby react-scripts.

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'assets'),
+    filename: 'index.js',
+    publicPath: 'assets/',
+  },
+  devServer: {
+    hot: true,
+    open: true,
+  },
+}
